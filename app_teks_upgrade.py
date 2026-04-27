24f384e51282cb75eae263ab56b6c77adf136dc7
andri <81407520+kianaputra@users.noreply.github.com>
2026-04-27 08:42:12 +0700

    Update app_teks_upgrade.py

--- app_teks_upgrade.py
+++ app_teks_upgrade.py
@@ -46,8 +46,7 @@ body { background-color: #fff1f2; }
 # =========================
 # TITLE
 # =========================
-col1, col2, col3 = st.columns([1,2,1])
-with col2:
+
     st.image("OEL60.png")
 
 st.title("AI Chat Sekolah ORA et LABORA")
