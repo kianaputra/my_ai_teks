39faaad4eda4a8dc297c3224e3496eff556adaaa
andri <81407520+kianaputra@users.noreply.github.com>
2026-05-01 14:00:35 +0700

    Update app_teks_upgrade.py

--- app_teks_upgrade.py
+++ app_teks_upgrade.py
@@ -131,7 +131,8 @@ def load_all_data(folder_path="data"):
     # 2. LOAD GOOGLE SHEET
     # =========================
     try:
-        sheet_url = "https://docs.google.com/spreadsheets/d/1suSM7789E8zsoPsb9YH0G1BIoKxi3nci02dLm5xBW2g/export?format=csv&gid=XXXX"
+        sheet_url = "https://docs.google.com/spreadsheets/d/1suSM7789E8zsoPsb9YH0G1BIoKxi3nci02dLm5xBW2g/export?format=csv"
+        sheet_url = "https://docs.google.com/spreadsheets/d/1FY9zao3G8oHuEttMAxKkpfIC2aET8BS_w3IuWgMfY14/export?format=csv"
         
         df = pd.read_csv(sheet_url)
 
